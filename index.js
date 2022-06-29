836299f1e3775fe819d44ab9b3de0fb4b02a64f7
Christian Trublet de Nermont <cjwtdn@gmail.com>
2022-06-29 11:43:23 -0600

    commit tree 05033ce4cee64fb59d388c48e1538cb239cbd35b

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+/*function findMatching(drivers, name){
+    for(items of drivers){
+        console.log(drivers.name)
+    }
+}*/
+
+
+function findMatching(source, sought) {
+    return source.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(source, testString) {
+    return source.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(source, soughtName) {
+    return source.filter((record) => record.name === soughtName);
+  }
